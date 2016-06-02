da87b0a2b7ae0672c6614d68b19b0b81f2c762f0
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2016-06-02 18:21:56 -0400

    Covers I, V, and X combinations.

--- lib/roman.ex
+++ lib/roman.ex
@@ -1,5 +1,20 @@
 defmodule Roman do
+
 	def roman number do
-		'I'
+		roman(number, "")
 	end	
+
+	defp roman(number, numeral) do
+
+		numeral = String.replace(numeral, "VIIII", "IX")
+		numeral = String.replace(numeral, "IIII", "IV")
+
+		case number do
+			number when number >= 10 -> roman(number - 10, numeral <> "X")
+			number when number >= 5 -> roman(number - 5, numeral <> "V")
+			number when number >= 1 -> roman(number - 1, numeral <> "I")
+			_ -> numeral
+		end
+	end
+
 end
--- test/roman_test.exs
+++ test/roman_test.exs
@@ -2,8 +2,47 @@ defmodule RomanTest do
   use ExUnit.Case
   doctest Roman
 
-	test "I is 1 in roman numerals" do
-		assert Roman.roman(1) == 'I'
+	test "1 is I in roman numerals" do
+		assert Roman.roman(1) == "I" 
 	end
 
+	test "2 is II in roman numerals" do
+		assert Roman.roman(2) == "II"
+	end
+
+	test "4 is IV in roman numerals" do
+		assert Roman.roman(4) == "IV"
+	end
+
+	test "5 is V in roman numerals" do
+		assert Roman.roman(5) == "V"
+	end
+
+	test "8 is VIII in roman numerals" do
+		assert Roman.roman(8) == "VIII"
+	end
+
+	test "9 is IX in roman numerals" do
+		assert Roman.roman(9) == "IX"
+	end
+
+	test "10 is X in roman numerals" do
+		assert Roman.roman(10) == "X"
+	end
+
+	test "14 is XIV in roman numerals" do
+		assert Roman.roman(14) == "XIV"
+	end
+
+	test "15 is XV in roman numerals" do
+		assert Roman.roman(15) == "XV"
+	end
+
+	test "19 is XIX in roman numerals" do
+		assert Roman.roman(19) == "XIX"
+	end
+
+	test "30 is XXX in roman numerals" do
+		assert Roman.roman(30) == "XXX"
+	end
 end
